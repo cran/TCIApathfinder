12712e2b8b021667cf5b77c2981eafe223493c64
Pamela Russell <pamela.russell@ucdenver.edu>
2018-01-24 22:54:33 +0000

    version 1.0.2

--- inst/doc/radiomics.R
+++ inst/doc/radiomics.R
@@ -6,16 +6,29 @@ if(identical(api_key, "")) {
 }
 
 ## ------------------------------------------------------------------------
-suppressPackageStartupMessages(library(radiomics))
+library(TCIApathfinder)
 
-# Pick one of the image slices
-img_array <- img_array_3d[, , 1]
-img_matrix <- matrix(img_array, dim(img_array))
+# Pick a patient of interest
+patient <- "TCGA-AR-A1AQ"
 
-# Calculate basic image features
-calc_features(img_matrix)
+# Get information on all image series for this patient
+series <- get_series_info(patient_id = patient)
 
-# Analyze the grey level co-occurrence matrix
-glcm <- glcm(img_matrix)
-calc_features(glcm)
+# Pick an image series to download
+series_instance_uid <- as.character(series$series[1, "series_instance_uid"])
+
+# Download and unzip the image series
+ser <- save_image_series(series_instance_uid = series_instance_uid, out_dir = "~/Desktop", out_file_name = "series1.zip")
+dicom_dir <- "~/Desktop/series1/"
+unzip("~/Desktop/series1.zip", exdir = dicom_dir)
+
+## ------------------------------------------------------------------------
+suppressPackageStartupMessages(library(oro.dicom))
+
+# Read in the DICOM images and create a 3D array of intensities
+dicom_list <- readDICOM(dicom_dir)
+img_array_3d <- create3D(dicom_list)
+
+# Check the dimensions of the 3D array
+dim(img_array_3d)
 
